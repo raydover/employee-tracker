689301c6ba8f2694789d00be403fc513d4d8d991
raydover <105178034+raydover@users.noreply.github.com>
2022-06-23 14:57:20 -0400

    save files, resintall packages

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (id, name)
+VALUES ("00001", "Engineer"),
+    
+       
+INSERT INTO role (id, title, salary, department_id)
+VALUES ("00011", "Manager", 00001),
+    
+       
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ("00111", "Sam", "Smith", "01111", "11111"),
+     
